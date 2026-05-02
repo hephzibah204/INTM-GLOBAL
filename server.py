47101e210546ceceaeb2a1eb1f482a663ff25d30
hephzibah204 <57093044+hephzibah204@users.noreply.github.com>
2026-05-02 23:00:50 +0100

    ok oo

--- server.py
+++ server.py
@@ -118,7 +118,10 @@ def init_db():
         defaults = [
             ("home", "home_hero_title", "Nourish Knowledge.<br><em>Transform Lives.</em>", "html"),
             ("home", "home_hero_sub", "The Institute of Nutritional Therapy Management (INTM) is a leading centre for evidence-based nutritional education, professional development, and transformative research across Africa and beyond.", "text"),
-            ("about", "about_intro", "The Institute of Nutritional Therapy Management (INTM Global) is a premier professional body dedicated to advancing the field of nutritional science.", "text")
+            ("about", "about_intro", "The Institute of Nutritional Therapy Management (INTM Global) is a premier professional body dedicated to advancing the field of nutritional science.", "text"),
+            ("contact", "contact_email", "info@intmglobal.org", "text"),
+            ("contact", "contact_phone", "08036210735", "text"),
+            ("contact", "contact_address", "Plot 6, Block B, Akinyode Family Layout, Unity Estate Off Akala Expressway, Zionist Estate, New Garage, Orita Challenge, Ibadan , Oyo State", "text")
         ]
         cur.executemany("INSERT INTO site_content (page_id, section_id, content, type) VALUES (?, ?, ?, ?)", defaults)
 
